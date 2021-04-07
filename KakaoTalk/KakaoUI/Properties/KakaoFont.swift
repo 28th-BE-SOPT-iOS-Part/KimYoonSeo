f71d6f292c3ffe2ba286c64ea9a062203d9bc1fc
김윤서 <yo26on@soongsil.ac.kr>
2021-04-07 21:35:31 +0900

    [feat] semibold, bold체 추가

--- KakaoTalk/KakaoUI/Properties/KakaoFont.swift
+++ KakaoTalk/KakaoUI/Properties/KakaoFont.swift
@@ -25,4 +25,36 @@ public extension UIFont{
     class var font22 : UIFont { UIFont.systemFont(ofSize: 22, weight: .regular) }
     class var font24 : UIFont { UIFont.systemFont(ofSize: 24, weight: .regular) }
     class var font26 : UIFont { UIFont.systemFont(ofSize: 26, weight: .regular) }
+    
+    //MARK:- SemiBold Font
+    class var font10Semibold : UIFont { UIFont.systemFont(ofSize: 10, weight: .semibold) }
+    class var font11Semibold : UIFont { UIFont.systemFont(ofSize: 11, weight: .bold) }
+    class var font12Semibold : UIFont { UIFont.systemFont(ofSize: 12, weight: .bold) }
+    class var font13Semibold : UIFont { UIFont.systemFont(ofSize: 13, weight: .bold) }
+    class var font14Semibold : UIFont { UIFont.systemFont(ofSize: 14, weight: .bold) }
+    class var font15Semibold : UIFont { UIFont.systemFont(ofSize: 15, weight: .bold) }
+    class var font16Semibold : UIFont { UIFont.systemFont(ofSize: 16, weight: .bold) }
+    class var font17Semibold : UIFont { UIFont.systemFont(ofSize: 17, weight: .bold) }
+    class var font18Semibold : UIFont { UIFont.systemFont(ofSize: 18, weight: .bold) }
+    class var font19Semibold : UIFont { UIFont.systemFont(ofSize: 19, weight: .bold) }
+    class var font20Semibold : UIFont { UIFont.systemFont(ofSize: 20, weight: .bold) }
+    class var font22Semibold : UIFont { UIFont.systemFont(ofSize: 22, weight: .bold) }
+    class var font24Semibold : UIFont { UIFont.systemFont(ofSize: 24, weight: .bold) }
+    class var font26Semibold : UIFont { UIFont.systemFont(ofSize: 26, weight: .bold) }
+    
+    //MARK:- Bold Font
+    class var font10Bold : UIFont { UIFont.systemFont(ofSize: 10, weight: .bold) }
+    class var font11Bold : UIFont { UIFont.systemFont(ofSize: 11, weight: .bold) }
+    class var font12Bold : UIFont { UIFont.systemFont(ofSize: 12, weight: .bold) }
+    class var font13Bold : UIFont { UIFont.systemFont(ofSize: 13, weight: .bold) }
+    class var font14Bold : UIFont { UIFont.systemFont(ofSize: 14, weight: .bold) }
+    class var font15Bold : UIFont { UIFont.systemFont(ofSize: 15, weight: .bold) }
+    class var font16Bold : UIFont { UIFont.systemFont(ofSize: 16, weight: .bold) }
+    class var font17Bold : UIFont { UIFont.systemFont(ofSize: 17, weight: .bold) }
+    class var font18Bold : UIFont { UIFont.systemFont(ofSize: 18, weight: .bold) }
+    class var font19Bold : UIFont { UIFont.systemFont(ofSize: 19, weight: .bold) }
+    class var font20Bold : UIFont { UIFont.systemFont(ofSize: 20, weight: .bold) }
+    class var font22Bold : UIFont { UIFont.systemFont(ofSize: 22, weight: .bold) }
+    class var font24Bold : UIFont { UIFont.systemFont(ofSize: 24, weight: .bold) }
+    class var font26Bold : UIFont { UIFont.systemFont(ofSize: 26, weight: .bold) }
 }
