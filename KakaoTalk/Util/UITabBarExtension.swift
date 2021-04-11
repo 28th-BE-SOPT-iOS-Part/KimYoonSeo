a3bf19b69e9c1624ed121844a2c48bf3d6b789d1
김윤서 <yo26on@soongsil.ac.kr>
2021-04-11 20:04:33 +0900

    [ADD] tabbar 관련 extension 추가

---
+++ KakaoTalk/Util/UITabBarExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UITabBarExtension.swift
+//  KakaoTalk
+//
+//  Created by 김윤서 on 2021/04/11.
+//
+
+import UIKit
+
+extension UITabBar{
+    func setUpUITabBar(){
+        self.backgroundImage = UIImage()
+        self.shadowImage = UIImage()
+        self.clipsToBounds = true
+    }
+}
---
+++ KakaoTalk/Util/UITabBarItemExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UITabBarItemExtension.swift
+//  KakaoTalk
+//
+//  Created by 김윤서 on 2021/04/11.
+//
+
+import UIKit
+
+extension UITabBarItem {
+    class var friendTab : UITabBarItem {UITabBarItem(title: nil, image: .profileTalkImg, tag: 0)}
+    class var talkTab   : UITabBarItem {UITabBarItem(title: nil, image: .profileStoryImg, tag: 0)}
+    class var hashTab   : UITabBarItem {UITabBarItem(title: nil, image: .profileEditImg, tag: 0)}
+    class var shopTab   : UITabBarItem {UITabBarItem(title: nil, image: .profileTalkImg, tag: 0)}
+    class var moreTab   : UITabBarItem {UITabBarItem(title: nil, image: .profileStoryImg, tag: 0)}
+}
+
