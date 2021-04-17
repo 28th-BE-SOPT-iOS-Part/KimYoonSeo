879679ff3fe02cb99849622cf461790e81a9c336
김윤서 <yo26on@soongsil.ac.kr>
2021-04-17 13:41:58 +0900

    [FEAT] HomeTabBarViewController tabbar item 위치 조정

--- KakaoTalk/Feature/Home/HomeTabBarViewController.swift
+++ KakaoTalk/Feature/Home/HomeTabBarViewController.swift
@@ -17,25 +17,26 @@ class HomeTabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpTabBarItem()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        view.backgroundColor = .white
         setUpTabBar()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setUpTabBarItem()
-    }
-    
+
     private func setUpTabBar(){
         tabBar.setUpUITabBar()
         tabBar.frame.size.height = 92
         tabBar.frame.origin.y = view.frame.height - 92
         tabBar.tintColor = .black
         tabBar.backgroundColor = .gray100
+        
+        [friendTab,talkTab,hashTab,shopTab,moreTab].forEach {
+            $0.tabBarItem.imageInsets = UIEdgeInsets(top: 10, left: 0, bottom: -10, right: 0)
+        }
+        
     }
     
     private func setUpTabBarItem(){
