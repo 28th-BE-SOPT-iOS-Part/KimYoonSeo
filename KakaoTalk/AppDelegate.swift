1adbd8ddf0131c4de1eb894a444ddbe322c009a9
김윤서 <yo26on@soongsil.ac.kr>
2021-05-09 23:29:34 +0900

    [ADD] AlamofireNetworkActivityLogger 팟 추가

--- KakaoTalk/AppDelegate.swift
+++ KakaoTalk/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireNetworkActivityLogger
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,6 +23,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.rootViewController = navigationController
         window?.makeKeyAndVisible()
         window?.backgroundColor = .white
+        
+        NetworkActivityLogger.shared.level = .debug
+        NetworkActivityLogger.shared.startLogging()
         
         return true
     }
--- Podfile
+++ Podfile
@@ -4,7 +4,9 @@
 target 'KakaoTalk' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-  pod 'SnapKit'	
+  pod 'SnapKit'
+  pod 'Alamofire','~>5.2'	
+  pod ‘AlamofireNetworkActivityLogger’, ‘~> 3.0’
   # Pods for KakaoTalk
 
 end
