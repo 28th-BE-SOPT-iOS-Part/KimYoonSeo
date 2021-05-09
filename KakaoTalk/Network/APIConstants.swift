44882a7c5d7d0f545c7dc901f0d3ab6a32ddda15
김윤서 <yo26on@soongsil.ac.kr>
2021-05-09 23:30:35 +0900

    [ADD] APIConstants 추가

---
+++ KakaoTalk/Network/APIConstants.swift
@@ -0,0 +1,20 @@
+//
+//  APIConstants.swift
+//  KakaoTalk
+//
+//  Created by 김윤서 on 2021/05/08.
+//
+
+import Foundation
+
+struct APIConstants{
+    
+    // MARK:- Base URL
+    static let baseURL = "http://cherishserver.com"
+    
+    
+    // MARK:- Feature URL
+    
+    static let signInURL = baseURL + "/login/signin"
+    static let signUpURL = baseURL + "/login/signup"
+}
